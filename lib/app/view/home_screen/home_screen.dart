118f211c7941ce460aa8760051289d4e6358abeb
Tulasi Reddy <tulasireddy.kagithala@karkinos.in>
2024-10-17 02:15:00 +0530

    Fixed AppBar Title Padding Issue

--- lib/app/view/home_screen/home_screen.dart
+++ lib/app/view/home_screen/home_screen.dart
@@ -1,10 +1,9 @@
-import 'package:talk_time/app/core/utils/enums.dart';
+import 'package:talk_time/app/core/utils/constants.dart';
 import 'package:talk_time/app/view/home_screen/widget/main_container_widget.dart';
 import 'package:talk_time/app/view/userslist_screen/user_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:talk_time/app/core/utils/color_palette.dart';
 import 'package:talk_time/app/core/utils/screen_sizes.dart';
-import 'package:talk_time/app/widget/app_icon_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -23,76 +22,81 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        bool isDesktopScreen = isDesktop(screenWidth: constraints.maxWidth);
-        return Scaffold(
-          key: _scaffoldKey,
-          backgroundColor: ColorPalette.whitePrimaryColor.shade100,
-          appBar: isDesktopScreen
-              ? null
-              : AppBar(
-                  backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-                  actions: const [],
-                  elevation: 0,
-                  title: const Padding(
-                    padding: EdgeInsets.all(20),
-                    child: AppIconWidget(),
+    return LayoutBuilder(builder: (context, constraints) {
+      bool isDesktopScreen = isDesktop(screenWidth: constraints.maxWidth);
+      return Scaffold(
+        key: _scaffoldKey,
+        backgroundColor: ColorPalette.whitePrimaryColor.shade100,
+        appBar: isDesktopScreen
+            ? null
+            : AppBar(
+                leadingWidth: 0,
+                backgroundColor: ColorPalette.primaryContainer,
+                actions: const [],
+                elevation: 6,
+                title: const Text(
+                  "TalkTime",
+                  softWrap: true,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: ColorPalette.purple,
+                    fontFamily: Constants.montserratBold,
                   ),
-                  centerTitle: true,
                 ),
-          extendBody: true,
-          extendBodyBehindAppBar: true,
-          drawer: Container(
-            decoration: BoxDecoration(
-              color: ColorPalette.primaryContainer, // Theme.of(context).colorScheme.primaryContainer,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            width: constraints.maxWidth - 80,
-            alignment: Alignment.topLeft,
-            padding: const EdgeInsets.only(top: 40, left: 14),
+                centerTitle: false,
+              ),
+        extendBody: true,
+        extendBodyBehindAppBar: true,
+        drawer: Container(
+          decoration: BoxDecoration(
+            color: ColorPalette.primaryContainer, // Theme.of(context).colorScheme.primaryContainer,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          width: constraints.maxWidth - 80,
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.only(top: 40, left: 14),
           child: UsersListScreen(
             maxWidth: (constraints.maxWidth - 80),
             isMobileScreen: true,
           ),
         ),
-          body: SafeArea(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Container(
-                      width: getCardWidth(screenWidth: constraints.maxWidth),
-                      margin: isDesktopScreen ? const EdgeInsets.only(left: 20, right: 20, bottom: 20) : null,
-                      child: LayoutBuilder(
-                        builder: (context, constraints) {
-                          return isDesktopScreen
-                              ? Card(
-                                  elevation: 0,
-                                  color: ColorPalette.whitePrimaryColor.shade100,
-                                  shape: const RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.only(
-                                      topRight: Radius.circular(20.0),
-                                      bottomRight: Radius.circular(20.0),
-                                    ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Container(
+                    width: getCardWidth(screenWidth: constraints.maxWidth),
+                    margin: isDesktopScreen ? const EdgeInsets.only(left: 20, right: 20, bottom: 20) : null,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return isDesktopScreen
+                            ? Card(
+                                elevation: 0,
+                                color: ColorPalette.whitePrimaryColor.shade100,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(20.0),
+                                    bottomRight: Radius.circular(20.0),
                                   ),
-                                  child: MainContainer(
-                                    maxWidthAndHeight: constraints,
-                                  ),
-                                )
-                              : MainContainer(
+                                ),
+                                child: MainContainer(
                                   maxWidthAndHeight: constraints,
-                                );
-                        },
-                      ),
+                                ),
+                              )
+                            : MainContainer(
+                                maxWidthAndHeight: constraints,
+                              );
+                      },
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        );
-      }
-    );
+        ),
+      );
+    });
   }
 }
