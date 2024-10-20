b68dc5a7d24fc85f1cfa346d35d917e165744025
Tulasi Reddy <tulasireddy.kagithala@karkinos.in>
2024-10-20 15:21:08 +0530

    Fixed null exception

--- lib/app/data/resource/local_db_contact_data_source.dart
+++ lib/app/data/resource/local_db_contact_data_source.dart
@@ -11,40 +11,29 @@ class LocalDbContactDataSource implements IContactDataSource {
   Future<List<UserContact>> fetchContacts() async {
     try {
       // Fetch contacts from local DB (e.g., SQLite or Hive)
-
-      List<String> dataList = [];
       if (!await FlutterContacts.requestPermission(readonly: true)) {
         _permissionDenied = true;
       } else {
         final contacts = await FlutterContacts.getContacts(withProperties: true, withPhoto: true);
         _contacts = contacts;
-        print("\nContacts List: ${_contacts?.length}");
-        print("\nContacts name: ${_contacts?[2].phones.first}");
-
-        _contacts?.forEach(
-                (element) {
-                  final String? num = (element.phones).isEmpty ? "No Number" : (element.phones.first.number);
-                  dataList.add(num ?? "kk");
-                });
-        print("\nContacts List: $dataList");
         _contacts?.forEach(
           (element) {
-
             final String name = "${element.name.first} ${element.name.last}";
-           // final String no = element.phones.first.number;
+            String no = "";
+            if (element.phones.isNotEmpty) {
+              no = element.phones.first.number;
+            }
 
             data.add(
               UserContact(
                 name: name,
-                //phoneNo: (no == null || no.isEmpty) ? "" : no,
+                phoneNo: no,
                 profilePic: element.photo,
                 dateTime: DateTime.now(),
               ),
             );
           },
         );
-
-
       }
       return data;
     } catch (e, m) {
